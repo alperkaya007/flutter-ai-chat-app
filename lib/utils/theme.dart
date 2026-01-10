d89e2de6da86c04edee682efbcf9a5ed4de08f6f
Alper Kaya <93725692+alperkaya007@users.noreply.github.com>
2026-01-10 12:00:00 +0000

    Add app theme configuration

---
+++ lib/utils/theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData get light => ThemeData(
+    colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF6750A4)),
+    useMaterial3: true,
+  );
+  static ThemeData get dark => ThemeData(
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: const Color(0xFF6750A4),
+      brightness: Brightness.dark,
+    ),
+    useMaterial3: true,
+  );
+}
